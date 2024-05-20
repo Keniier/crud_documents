f7439a590c57f0f8eaec42fd3a4f5ba9c79450a3
KeniierM <keniier.321@gmail.com>
2024-05-20 12:42:58 -0500

    Implementacion de vue y configuracion de vista

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import DocumentIndex from './components/document/Index.vue';
+
+const app = createApp({});
+
+app.component('document-index', DocumentIndex);
+
+app.mount('#app');
